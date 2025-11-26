d174ecace09e053b89b35caaba3a8b28af16de83
Antonio Caria <antonio.caria-1@studenti.unitn.it>
2025-11-26 13:39:17 +0100

    added test

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080
 // Handling GET requests
 app.get('/', function(req, res){
 	res.send('Hello World!');
+    console.log('Hello World!');
 });
 //
 app.listen(PORT, function() {
