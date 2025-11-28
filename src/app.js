899457f14907ff88f816d5cbd7d3735d3af93a73
Antonio Caria <antonio.caria-1@studenti.unitn.it>
2025-11-28 17:54:57 +0100

    created a test v1/orti api call

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080
 
+
+
+
+// Handling GET requests
+app.get('/api/v1/orti', function(req, res){
+	if(req.params.sensor)
+		res.json()
+	res.status(200).json({"name": "povo1", "association": {name: "Amici di Povo"}, "latitude": "1234", "longitude": "5678"});
+    console.log('/api/v1/orti');
+});
+
 // Handling GET requests
 app.get('/', function(req, res){
 	res.send('Hello World!');
     console.log('Hello World!');
 });
+
 //
 app.listen(PORT, function() {
 	console.log('Server running on port ', PORT);
