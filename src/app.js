7bbed110d557d24d798403de72a659b8e968cb42
Antonio Caria <antonio.caria-1@studenti.unitn.it>
2025-11-28 17:54:57 +0100

    created a test v1/orti api call

--- src/app.js
+++ src/app.js
@@ -13,6 +13,17 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const PORT = process.env.PORT || 8080
 
+
+
+
+// Handling GET requests
+app.get('/api/v1/orti', function(req, res){
+	if(req.params.sensor)
+		res.json()
+	res.status(200).json({"name": "povo1", "association": {name: "Amici di Povo"}, "latitude": "1234", "longitude": "5678"});
+    console.log('/api/v1/orti');
+});
+
 // Handling GET requests
 app.get('/', async function(req, res){
 	res.send('Hello World!');
