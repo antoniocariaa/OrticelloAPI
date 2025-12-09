8c7d5f842284088178f0fe177b1f7285eb3acd72
Antonio Caria <antonio.caria-1@studenti.unitn.it>
2025-12-09 13:41:50 +0100

    fixed mail, phone number in associazione/comune.js

--- src/model/org/associazione.js
+++ src/model/org/associazione.js
@@ -4,14 +4,27 @@ const { Schema } = mongoose;
 const associazioneSchema = new Schema({
   nome: { type: String, required: true, trim: true },
   indirizzo: { type: String, required: true, trim: true },
-  telefono: { type: String, required: true, trim: true },
-  mail: {
+  telefono: {
     type: String,
     required: true,
-    trim: true,
-    lowercase: true,
-    match: [/.+@.+\..+/, "Formato email non valido"]
-  }
+    validate:{
+        validator: function(v){
+            return validator.isMobilePhone(v);
+        },
+        message: '{VALUE} non è un numero di telefono valido'
+    } 
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+        validator: function(v) {
+        return validator.isEmail(v);
+        },
+        message: '{VALUE} non è un\'email valida'
+    }
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Associazione", associazioneSchema, "associazione");
