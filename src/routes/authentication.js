7e94575331d785e31098a33038567e8ac9555a2a
antoniocariaa <ant8.caria@gmail.com>
2025-12-13 14:14:49 +0100

    started GoogleAuth implementation, to be Tested

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,16 +1,45 @@
 var express = require('express');
 var Utente = require('../model/utente');
 var jwt = require('jsonwebtoken');
+var OAuth2Client = require('google-auth-library');
 
 const router = express.Router();
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
+const client = new OAuth2Client( GOOGLE_CLIENT_ID );
+
+async function verify( token ) {
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		// audience: GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+		// Or, if multiple clients access the backend:
+		//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+	});
+	const payload = ticket.getPayload();
+	const userid = payload['sub'];
+	// If the request specified a Google Workspace domain:
+	// const domain = payload['hd'];
+	return payload;
+}
 
 router.post('', async function(req, res) {
     var user = {};
 
     if(req.body.googleToken){
-        // Handle Google token authentication
-        // TO DO LATER
+        const payload = await verify( req.body.googleToken ).catch(console.error);
+		console.log(payload);
+
+		user = await Utente.findOne({ email: payload['email'] }).exec();
+		if ( ! user ) {
+			user = new Utente({
+				email: payload['email'],
+				password: 'default-google-password-to-be-changed'
+			});
+			await user.save().exec();
+			console.log('Utente created after login with google');
+			
+		}
     }else{
         console.log(req.body.email);
         user = await Utente.findOne({ email: req.body.email }).select('email password').exec();
